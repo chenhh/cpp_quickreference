30156f62df7d2454fb5168b8db45b949c1e5eff0
chenhh <chenhh@x220>
2017-07-21 19:35:00 +0800

    test commit

--- const_example.cpp
+++ const_example.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <cstdlib>
+
 using namespace std;
 
-class Point{
+class Point {
     // constructor
-    Point(): x(0), y(0){}
-    Point(int lx, int ly):x(lx), y(ly){}
-    Point (const Point& rpoint){
-        this->x=rpoint.x;
-        this->y=rpoint.y;
+    Point() : x(0), y(0) {}
+
+    Point(int lx, int ly) : x(lx), y(ly) {}
+
+    Point(const Point &rpoint) {
+        this->x = rpoint.x;
+        this->y = rpoint.y;
     }
 
     //assignment
-    Point operator=(const Point& rpoint){
-        this->x=rpoint.x;
-        this->y=rpoint.y;
+    Point operator=(const Point &rpoint) {
+        this->x = rpoint.x;
+        this->y = rpoint.y;
     }
 
     // const membership function
-    int getX() const{
+    int getX() const {
         return this->x;
     }
 
-    double square_sum() const{
-        return x*x+y*y;
+    double square_sum() const {
+        return x * x + y * y;
     }
 
     // non-const membership function
@@ -32,8 +35,8 @@ class Point{
     }
 
 private:
-    int x=0;
-    int y=0;
+    int x = 0;
+    int y = 0;
 };
 
 
@@ -43,15 +46,15 @@ int main() {
      */
     const int v1 = 10;
     // v1 = 20; error
-    cout<<"const variable v1="<<v1<<endl;
-    
+    cout << "const variable v1=" << v1 << endl;
+
     /*
      * reference to const variable
      */
     // int &v2 = v1; error
-    const int& v2 = v1;
+    const int &v2 = v1;
     // v2 = 20; error
-    cout<<"const reference to v1, v2="<<v2<<endl;
+    cout << "const reference to v1, v2=" << v2 << endl;
 
 
     /*
@@ -60,17 +63,17 @@ int main() {
      * but the pointer cannot point another variable
      */
     int v3 = 30;
-    int* const pv3 = &v3;
+    int *const pv3 = &v3;
 
     // the value can be changed by itself
     v3 = 33;
     // v3=33, pv3=33
-    cout<<"v3="<<v3<<", pv3="<<*pv3<<endl;
+    cout << "v3=" << v3 << ", pv3=" << *pv3 << endl;
 
     // the value can be changed from the pointer
     *pv3 = 36;
     // v3=36, pv3=36
-    cout<<"v3="<<v3<<", pv3="<<*pv3<<endl;
+    cout << "v3=" << v3 << ", pv3=" << *pv3 << endl;
 
     // the pointer cannot point a new value
     int v33 = 39;
@@ -83,10 +86,10 @@ int main() {
      * but the pointer can point to another variable
      */
     int v4 = 40;
-    const int* pv4 = &v4;
+    const int *pv4 = &v4;
     v4 = 44;
     // the value can be changed by itself
-    cout<<"v4="<<v4<<", pv4="<<*pv4<<endl;
+    cout << "v4=" << v4 << ", pv4=" << *pv4 << endl;
 
     // the value cannot be changed by the pointer
     //*pv4 = 46;  //error
@@ -95,13 +98,13 @@ int main() {
     int v44 = 444;
     // v4=44, pv4=444
     pv4 = &v44;
-    cout<<"v4="<<v4<<", pv4="<<*pv4<<endl;
+    cout << "v4=" << v4 << ", pv4=" << *pv4 << endl;
 
     /*
      * const char
      */
-    const char* const str="hello world";
-    cout<<str<<endl;
+    const char *const str = "hello world";
+    cout << str << endl;
 
 
     return EXIT_SUCCESS;
