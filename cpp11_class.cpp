daea9316b6c6b297219cc60723bbe86ce11dac20
chenhh <chenhh@x220>
2017-07-22 02:10:55 +0800

    simple read file

--- cpp11_class.cpp
+++ cpp11_class.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdlib>
+#include <exception>
+#include <stdexcept>
 
 using namespace std;
 
 class Parent {
 public:
 
-    Parent(){}
+    Parent() {}
+
     void func1() {
         cout << "func1 parent" << endl;
     }
@@ -15,12 +18,12 @@ public:
         cout << "func2 in parent" << endl;
     }
 
-    virtual void func3() final{
-        cout<<"func3 in parent"<<endl;
+    virtual void func3() final {
+        cout << "func3 in parent" << endl;
     }
 
-    Parent(const Parent&) = delete; //delete copy constructor
-    Parent& operator=(const Parent&) = delete; //delete assigment operator
+    Parent(const Parent &) = delete; //delete copy constructor
+    Parent &operator=(const Parent &) = delete; //delete assigment operator
 };
 
 class Child1 : public Parent {
@@ -29,20 +32,53 @@ public:
         cout << "func1 child1" << endl;
     }
 
-    void func2() override{
+    void func2() override {
         cout << "func2 in child1" << endl;
     }
-    
-    void func3() const{
+
+    void func3() const {
         // the function does not override parent function
-        cout<<"const func3 in child1"<<endl;
+        cout << "const func3 in child1" << endl;
     }
 };
 
+class DynamicArr {
+public:
+    DynamicArr(const int len)
+    try : _len(len) {
+        parr = new int(len);
+    } catch (runtime_error &e) {
+        cerr << e.what() << endl;
+        exit(-1);
+    }
+
+    void show(){
+        for (int idx = 0; idx < _len; ++idx) {
+            cout<<*(parr+idx)<<endl;
+        }
+    }
+
+    ~DynamicArr() {
+        /*
+         * https://wizardforcel.gitbooks.io/effective-cpp/content/10.html
+         */
+        try {
+            delete parr;
+            parr = nullptr;
+        } catch (exception &e) {
+            exit(-1);
+        }
+    }
+
+private:
+    int *parr = nullptr;
+    int _len = 0;
+};
+
 int main() {
-    Parent* obj1 = new Parent();
-    Parent* obj2 = new Child1();
-    Child1* obj3 = new Child1();
+    Parent *obj1 = new Parent();
+    Parent *obj2 = new Child1();
+    Child1 *obj3 = new Child1();
 
     obj1->func1();  // func1 parent
     obj2->func1();  // func1 parent
@@ -54,5 +90,8 @@ int main() {
     obj2->func3();  // func in parent
     obj3->func3();  // const func3 in child1
 
+
+    DynamicArr myarr(10);
+    myarr.show();
     return EXIT_SUCCESS;
 }
