c38347b4675d6525a7ca152f485fa4f977b75bb2
chenhh <chenhh@x220>
2017-07-21 18:47:30 +0800

    const example

--- const_example.cpp
+++ const_example.cpp
@@ -1,4 +1,37 @@
-//
-// Created by chen1 on 2017/7/21.
-//
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main() {
+    /*
+     * const variable, it cannot be changed.
+     */
+    const int v1 = 10;
+    // v1 = 20; error
+    cout<<"const variable v1="<<v1<<endl;
+    
+    /*
+     * reference to const variable
+     */
+    // int &v2 = v1; error
+    const int& v2 = v1;
+    // v2 = 20; error
+    cout<<"const reference to v1, v2="<<v2<<endl;
+
+
+    /*
+     * const pointer to a const variable
+     */
+    const int* const p1 = &v1;
+    cout<<"const pointer to a const variable, p1="<<*p1<<endl;
+
+
+    /*
+     * const char
+     */
+    char* const str="hello world";
+    cout<<str<<endl;
+
+
+    return EXIT_SUCCESS;
+}
